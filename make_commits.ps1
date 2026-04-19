f30cca3512296fbb7576b81a2851c5b867ea3d1c
prthmesh <prathmeshl.cs25@sbjit.com>
2026-04-19 14:11:49 +0530

    Remove script

--- make_commits.ps1
+++
@@ -1,8 +0,0 @@
-cd "c:\Users\ADMIN\MyProject"
-for ($i = 2; $i -le 15; $i++) {
-    $content = Get-Content WDL_EXPERIENTIAL_LEARNING.html
-    $content[-2] = "<!-- Commit $i -->"
-    $content | Set-Content WDL_EXPERIENTIAL_LEARNING.html
-    git add .
-    git commit -m "Commit $i"
-}
